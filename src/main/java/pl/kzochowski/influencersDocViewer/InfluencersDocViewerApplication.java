604a0ce96a511f8deb1b7dffebaaa1fd8ad9f626
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-12-02 00:16:14 +0100

    initial commit

---
+++ src/main/java/pl/kzochowski/influencersDocViewer/InfluencersDocViewerApplication.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.influencersDocViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencersDocViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluencersDocViewerApplication.class, args);
+	}
+
+}
